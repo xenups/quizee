125119ed966f86780d2f4242acff7746ab87b330
xenups <xenups@outlook.com>
2019-09-21 10:56:47 +0430

    create account model and view

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-
-    # path('accounts/', include('rest_registration.api.urls')),
+    path('registration/', cache_page(1 * 1)(views.UserProfileViewSet.as_view())),
+    path('usersProfile/<int:pk>/', views.UserProfileDetail.as_view()),
+    path('uploadProfileImage/', cache_page(60 * 60)(views.ProfileImageViewSet.as_view())),
 
 ]
